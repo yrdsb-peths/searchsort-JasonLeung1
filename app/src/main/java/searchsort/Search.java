f8c43982a37e66e9ae8e26a6f04f777e5aedcc42
JasonLeung1 <440004783@gapps.yrdsb.ca>
2024-12-10 11:22:11 +0800

    Update Search.java

--- app/src/main/java/searchsort/Search.java
+++ app/src/main/java/searchsort/Search.java
@@ -3,26 +3,49 @@ package searchsort;
 /**
  * Abstract class for search algorithms.
  */
-public abstract class Search {
+public class BinarySearch extends Search {
+
     /**
      * Find the index of the target element in the sorted array arr using binary
      * search. Return -1 if target is not found in arr.
      */
-    abstract public int find(int[] arr, int target);
+    public int find(int[] arr, int target) {
+        int left = 0, right = arr.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
 
     /**
      * Recursively find the index of the target element in the sorted array arr
      * using binary. Return -1 if target is not found in arr.
      */
-    abstract public int recursiveFind(int[] arr, int target);
+    abstract public int recursiveFind(int[] arr, int target) {
+        return binarySearchRecursive(arr, target, 0, arr.length - 1);
+    }
 
     /**
      * Return the time taken to execute a search operation.
      */
-    protected long timedFind(int[] arr, int target) {
-        long startTime = System.nanoTime();
-        find(arr, target);
-        long endTime = System.nanoTime();
-        return endTime - startTime;
+    private int binarySearchRecursive(int[] arr, int target, int left, int right) {
+        if (left > right) {
+            return -1;
+        }
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == target) {
+            return mid;
+        } else if (arr[mid] < target) {
+            return binarySearchRecursive(arr, target, mid + 1, right);
+        } else {
+            return binarySearchRecursive(arr, target, left, mid - 1);
+        }
     }
 }
